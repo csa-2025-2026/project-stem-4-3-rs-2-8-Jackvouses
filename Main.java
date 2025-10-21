2ca1c8d63166b324481685f82c8d87be414c4aa3
Jackvouses <jackvo756@gmail.com>
2025-10-21 18:21:40 +0000

    uwa

--- Main.java
+++ Main.java
@@ -5,5 +5,20 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    //Problem 1:
+    for (int count = 1; count <= 25; count += 2)
+    {
+      System.out.println(count);
+    }
+
+    //Problem 2:
+    for (int wawa = 17; wawa <= 73; wawa++)
+    {
+      if (wawa % 10 == 6)
+      {
+         System.out.println();
+      }
+    }
+    System.out.println(wawa + " ");
   }
 }
