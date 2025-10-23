6f06dad9c4de1faf6e88c7509926c6b13c681d91
Jackvouses <jackvo756@gmail.com>
2025-10-23 18:20:05 +0000

    bo

--- Main.java
+++ Main.java
@@ -10,15 +10,36 @@ public class Main
     {
       System.out.println(count);
     }
-
     //Problem 2:
     for (int wawa = 17; wawa <= 73; wawa++)
     {
+      System.out.print(wawa + " ");
       if (wawa % 10 == 6)
       {
-         System.out.println();
+        System.out.println();
+        }
+    }
+    //Problem 3:
+    Scanner sc = new Scanner(System.in);
+    int baubau;
+    System.out.println("Enter a number between 0 and 50:");
+    baubau = sc.nextInt();
+    if (!(0 <= baubau && baubau <= 50))
+    {
+      System.out.println("error");
+    }
+    else
+    {
+      for (int konkon = baubau; konkon <= 50; konkon++)
+      {
+        System.out.print(konkon + " ");
+        if (konkon % 5 == (baubau + 4) % 5)
+        {
+          System.out.println();
+        }
       }
     }
-    System.out.println(wawa + " ");
+    //Problem 4:
+    
   }
-}
+}  
